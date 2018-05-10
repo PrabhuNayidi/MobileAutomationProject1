f8643e8573b82ede1049dfa4ea19de22a6157623
Prabhu Nayidi <pabhun.python@gmail.com>
2018-05-10 11:16:46 -0500

    New Comment was added to the SampleCLass1 file.

--- MobileAutomationProject1/src/com/basics/SampleClass1.java
+++ MobileAutomationProject1/src/com/basics/SampleClass1.java
@@ -5,6 +5,8 @@ public class SampleClass1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to Mobile Automation Testing Class!!!");
+		
+		System.out.println("Thank you for Registering to my Class");
 	}
 
 }
